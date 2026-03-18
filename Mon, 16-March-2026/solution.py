515248e7db02c3a42ef92ab7dc4d6af4b43b07c0
Devesh Joshi <142724201+devesh-joshi52@users.noreply.github.com>
2026-03-18 10:21:45 +0530

    Add files via upload

---
+++ Mon, 16-March-2026/solution.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+driver = webdriver.Chrome()
+
+driver.get("https://opensource-demo.orangehrmlive.com/")
+driver.maximize_window()
+
+sleep(2)
+
+print("Page Title:", driver.title)
+
+username = driver.find_element(By.NAME, "username")
+username.clear()
+
+username.send_keys("Admin")
+
+password = driver.find_element(By.NAME, "password")
+password.send_keys("admin123")
+
+login_btn = driver.find_element(By.XPATH, "//button[@type='submit']")
+login_btn.click()
+
+sleep(3)
+
+current_url = driver.current_url
+print("Current URL:", current_url)
+
+if "dashboard" in current_url.lower():
+    print("Successful Login")
+else:
+    print("Login Failed")
+
+driver.quit()
